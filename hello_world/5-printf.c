feb6676a5af73525a880d7d0a6c6370df2694b4b
Thorgalix <lucaspodevin@sfr.fr>
2025-10-20 12:25:51 +0000

    Ex5 Hello, printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - fonction
+*
+* Fonction du programme : Affiche with proper grammar, but the outcome is a piece of art,
+* suivi d'une ligne vide
+*
+* Return: 0 si réussi
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
--- hello_world/README.md
+++ hello_world/README.md
@@ -2,4 +2,5 @@
 1.Compiler = gcc -c "$CFILE" -o "${CFILE%.C}.o"
 2.Assembler = gcc -S "$CFILE" -o "${CFILE%.c}.s"
 3.Name = gcc $CFILE -o cisfun
-4.Hello, puts = int main (void) 
+4.Hello, puts = int main (void) puts
+5.Hello, printf = int main (void) printf \n
