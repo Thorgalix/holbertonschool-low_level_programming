d1da190993b28f8da37462af70c044e5f7e2c5b2
Thorgalix <lucaspodevin@sfr.fr>
2025-10-20 10:30:47 +0000

    with commentary

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+/**
+* main - fonction
+*
+* Fonction du programme : Affiche "Programming is like building a multilingual 
+* puzzle suivi d'une ligne vide
+*
+* Return : 0 si réussi
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
