137eab8674a67ade0b9f027d2911c3182ac298ed
Thorgalix <lucaspodevin@sfr.fr>
2025-10-21 08:41:43 +0000

    Ex1

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - fonction
+*
+* Fonction du programme: Affiche le dernier chiffre d'un nombre aléatoires
+* si >5 is greater than 5
+* si 0 is 0
+* else is less than 6 and not 0
+*
+* Return: 0 si réussi
+*/
+int main(void)
+{
+int n, last;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+last = n % 10;
+printf("Last digit of %d is %d", n, last);
+if (last > 5)
+{
+printf(" and is greater than 5\n");
+}
+else if (last == 0)
+{
+printf(" and is 0\n");
+}
+else
+printf(" and is less than 6 and no 0\n");
+return (0);
+}
--- variables_if_else_while/README.md
+++ variables_if_else_while/README.md
@@ -1,1 +1,2 @@
 Ex0=if,else if, else
+Ex1=same with last digit
