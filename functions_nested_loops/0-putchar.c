97ccc852a5597ee4d8a072e234d823c19e7c1006
Thorgalix <lucaspodevin@sfr.fr>
2025-10-22 10:30:06 +0200

    Ex0 program _putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,6 +1,24 @@
-#include <main.h>
+#include "main.h"
 
+/**
+* main - fonction
+*
+* Fonction du programme : 
+* 
+*
+* Return: 0 si réussi
+*/
+int main(void)
 {
+	char p = "_putchar";
+	int i = 0;
+	while (i < 8)
+	{
+		_putchar(p);
+		i++;
+	}
+
+	_putchar('\n');
 
 	return (0);
 }
