aac57459e9ca546e07948278fdd8a053a54994ca
Thorgalix <lucaspodevin@sfr.fr>
2025-10-22 13:28:45 +0200

    Ex1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
-* main - fonction _putchar
+* print_alphabet - Prints the alphabet in lowercase, followed by a new line.
 *
-* Fonction du programme : affiche _putchar
-*
-* Return: 0 si réussi
+* Description: This function uses a loop to print
+* all lowercase letters from 'a' to 'z' using _putchar,
+* and then prints a newline character.
 */
 void print_alphabet(void)
 {
