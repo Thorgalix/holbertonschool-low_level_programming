9045d0e4b8b48341482579b66a919ea72ae62bea
Thorgalix <lucaspodevin@sfr.fr>
2025-10-23 15:29:15 +0200

    11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - for print at 98
+ * @n : is the first value
+*/
+void print_to_98(int n)
+
+{
+	while (n != 98)
+	{
+		printf("%d, ", n);
+		if (n < 98)
+		n++;
+		else
+		n--;
+	}
+	printf("98\n");
+}
