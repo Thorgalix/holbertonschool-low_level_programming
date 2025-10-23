c58d66ac90eeecdd12e145503455a9e44541e122
Thorgalix <lucaspodevin@sfr.fr>
2025-10-23 11:15:54 +0200

    Ex10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds 2 variables
+ * @a: the first integer
+ * @b: the second integer
+ *
+ * Return: the sum of a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
