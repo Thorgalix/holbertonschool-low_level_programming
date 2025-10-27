08b0a56a614b5d0522502da4136a78a1d4d6aa95
Thorgalix <lucaspodevin@sfr.fr>
2025-10-27 15:07:28 +0100

    Ex9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,15 +1,10 @@
 #include <stdio.h>
-/*
- * main - programme qui affiche les nombres de 1 à 100
- *         en remplaçant certains nombres par des mots :
- *         - multiples de 3 : "Fizz"
- *         - multiples de 5 : "Buzz"
- *         - multiples de 3 et 5 : "FizzBuzz"
- * Chaque élément est séparé par un espace et un retour à la ligne
- * est ajouté à la fin.
+/**
+ * main - prints the numbers from 1 to 100 with Fizz, Buzz, FizzBuzz
+ *
+ * Return: Always 0
  */
-int main (void)
-
+int main(void)
 {
 	int i = 1;
 
