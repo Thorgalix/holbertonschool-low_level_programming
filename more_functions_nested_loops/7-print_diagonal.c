7a42d1a08f9c1451f723d0466660a5b9bf14ba42
Thorgalix <lucaspodevin@sfr.fr>
2025-10-27 12:50:33 +0100

    ex7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* print_diagonal - Function for print _
+* @n: integer
+*/
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 2; j <= i; j++)
+			{
+				_putchar(' ');
+			}
+		_putchar('\\');
+		_putchar('\n');
+		}
+	}
+}
