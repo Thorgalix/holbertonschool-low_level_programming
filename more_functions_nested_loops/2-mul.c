8385487d3c9c39c12b7d6125539b5b5226fec0d7
Thorgalix <lucaspodevin@sfr.fr>
2025-10-27 10:01:14 +0100

    Ex2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* mul - Function for multiply 2 int
+*
+* @a: first int
+* @b: second int
+* Return: multiply a and b
+*/
+
+int mul(int a, int b)
+
+{
+	return (a * b);
+}
+
