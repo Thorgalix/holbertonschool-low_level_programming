abc79836aba7396fab07cb40ca2e390c6a972f1d
Thorgalix <lucaspodevin@sfr.fr>
2025-10-27 09:39:42 +0100

    Ex0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_alphabet - Prints the alphabet in lowercase, followed by a new line.
+*
+* Description: This function uses a loop to print
+* all lowercase letters from 'a' to 'z' using _putchar,
+* and then prints a newline character.
+*/
+
+int _isupper(int c)
+
+{
+	if (c >= 'A' && c <= 'Z')
+	return (1);
+	else
+	return (0);
+}
