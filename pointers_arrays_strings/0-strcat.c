d7a1b7da2fcc18112e6b3246748ae7bca30ae4aa
Thorgalix <lucaspodevin@sfr.fr>
2025-10-29 11:03:59 +0100

    Ex0Part3

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two strings
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j;
+
+	while (dest[i])
+	{
+		i++;
+	}
+	for (j = 0; src[j]; j++, i++)
+	{
+		dest[i] = src[j];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
