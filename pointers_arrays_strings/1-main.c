272cf612df7b8778cbd0fab2b688a029a670807c
Thorgalix <lucaspodevin@sfr.fr>
2025-10-29 11:39:47 +0100

    Ex1Part3

--- pointers_arrays_strings/1-main.c
+++ pointers_arrays_strings/1-main.c
@@ -1,4 +1,3 @@
-Tasks
 #include "main.h"
 #include <stdio.h>
 
@@ -15,7 +14,11 @@ char *ptr;
 
 printf("%s\n", s1);
 printf("%s", s2);
-ptr = _strcat(s1, s2);
+ptr = _strncat(s1, s2, 1);
+printf("%s\n", s1);
+printf("%s", s2);
+printf("%s\n", ptr);
+ptr = _strncat(s1, s2, 1024);
 printf("%s", s1);
 printf("%s", s2);
 printf("%s", ptr);
