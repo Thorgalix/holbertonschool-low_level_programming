a179e742125f36a32732810fd46488c87721942e
Thorgalix <lucaspodevin@sfr.fr>
2025-10-29 09:55:26 +0100

    Ex10

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * _atoi - convert a string to an integer
+ * @s: pointer to string
+ *
+ * Return: the integer value
+ */
+int _atoi(char *s)
+{
+	int i = 0;
+	int sign = 1;
+	int num = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+		{
+			sign *= -1;
+		}
+		else if (s[i] == '+')
+		{
+			;
+		}
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			num = num * 10 + (s[i] - '0');
+		}
+		else if (num > 0)
+		{
+			break;
+		}
+		i++;
+	}
+	return (num * sign);
+}
