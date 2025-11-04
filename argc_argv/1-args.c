2447176c209814227bfc8921da84fea411a37ca7
Thorgalix <lucaspodevin@sfr.fr>
2025-11-04 16:35:53 +0100

    Ex1P3

---
+++ argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the name of the program
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d", argc);
+	return (0);
+}
