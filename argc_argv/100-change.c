ab76bf2c671561dfbd812cb4b8c055ef68724609
Thorgalix <lucaspodevin@sfr.fr>
2025-11-06 20:49:58 +0100

    AdvP3

---
+++ argc_argv/100-change.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point of the program
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int centimes, piece = 0;
+
+	centimes = atoi(argv[1]);
+	if (centimes < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		for (; centimes >= 25; piece++)
+		{
+			centimes -= 25;
+		}
+		for (; centimes >= 10; piece++)
+		{
+			centimes -= 10;
+		}
+		for (; centimes >= 5; piece++)
+		{
+			centimes -= 5;
+		}
+		for (; centimes >= 2; piece++)
+		{
+			centimes -= 2;
+		}
+		for (; centimes >= 1; piece++)
+		{
+			centimes -= 1;
+		}
+		printf("%d\n", piece);
+		return (0);
+	}
+}
