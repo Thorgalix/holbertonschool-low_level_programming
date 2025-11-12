5feb8593557a430d3fc6fb9759684401e7929b7a
Thorgalix <lucaspodevin@sfr.fr>
2025-11-12 10:48:57 +0100

    Ex2P2

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _calloc - allocates memory for an array, using malloc
+ * @nmemb: number of elements
+ * @size: size of each element
+ *
+ * Return: pointer to the allocated memory
+ *         NULL if nmemb or size is 0
+ *         NULL if malloc fails
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *p;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	p = malloc(nmemb * size);
+	if (p == NULL)
+	return (NULL);
+	for (i = 0; i < (nmemb * size); i++)
+	{
+		((char *)p)[i] = 0;
+	}
+	return (p);
+}
