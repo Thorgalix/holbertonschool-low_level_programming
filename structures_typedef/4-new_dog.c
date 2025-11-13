bc668163a3af127a9632917d1eec015b41b2343b
Thorgalix <lucaspodevin@sfr.fr>
2025-11-13 11:21:46 +0100

    Ex4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -13,15 +13,23 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	int l, i;
+	dog_t *ndog;
 
-	dog_t *ndog = malloc(sizeof(dog_t));
+	if (name == NULL || owner == NULL)
+	return (NULL);
 
+	ndog = malloc(sizeof(dog_t));
 	if (ndog == NULL)
 	return (NULL);
 
 	for (l = 0; name[l] != '\0'; l++)
 	;
 	ndog->name = malloc(l + 1);
+	if (ndog->name == NULL)
+	{
+		free(ndog);
+		return (NULL);
+	}
 	for (i = 0; i <= l; i++)
 	{
 		ndog->name[i] = name[i];
@@ -30,17 +38,15 @@ dog_t *new_dog(char *name, float age, char *owner)
 	for (l = 0; owner[l] != '\0'; l++)
 	;
 	ndog->owner = malloc(l + 1);
+	if (ndog->owner == NULL)
+	{
+		free(ndog->name);
+		free(ndog);
+		return (NULL);
+	}
 	for (i = 0; i <= l; i++)
 	{
 		ndog->owner[i] = owner[i];
-	}
-
-	if (ndog->name == NULL || ndog->owner == NULL)
-	{
-		free(ndog->name);
-		free(ndog->owner);
-		free(ndog);
-		return (NULL);
 	}
 	ndog->age = age;
 	return (ndog);
