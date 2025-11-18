301f2a44d76530a03c968ec8f939a6aad16120af
Thorgalix <lucaspodevin@sfr.fr>
2025-11-18 17:38:37 +0100

    FIX Ex3P1

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -10,7 +10,7 @@
  */
 int (*get_op_func(char *s))(int, int)
 {
-	int i;
+	int i = 0;
 
 	op_t ops[] = {
 		{"+", op_add},
@@ -20,12 +20,14 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-	for (i = 0; i < 5; i++)
+
+	while (i < 5)
 	{
 		if (strcmp(s, ops[i].op) == 0)
 		{
 			return (ops[i].f);
 		}
+		i++;
 	}
 	return (NULL);
 }
