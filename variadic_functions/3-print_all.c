02e6add46c22c187546055d3fc2e701d4bfd752d
Thorgalix <lucaspodevin@sfr.fr>
2025-11-19 17:47:08 +0100

    Améliore la fonction print_all pour gérer les types de données variés

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -5,11 +5,39 @@
 void print_all(const char * const format, ...)
 {
 	va_list args;
-	int i = 0;
-	char *s;
+	unsigned int i = 0;
+	char *str;
+	double f;
+	int num;
 
 	va_start(args, format);
 
+	while (format != NULL && format[i] != '\0')
+	{
+		if (format[i] == 'c')
+		{
+			printf("%c", va_arg(args, int));
+		}
+		if (format[i] == 'i')
+		{
+			printf("%d", va_arg(args, int));
+		}
+
+		if (format[i] == 's')
+		{
+			str = va_arg(args, char *);
+			printf("%s", str ? str : "(nil)");
+
+		}
+		if (format[i] == 'f')
+		{
+			f = va_arg(args, double);
+			printf("%f", f ? f : "(nil)");
+		}
+
+
+		i++;
+	}
 
 	va_end(args);
 	printf("\n");
