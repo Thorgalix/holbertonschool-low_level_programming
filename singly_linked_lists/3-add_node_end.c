dc257e5ee048382517b9f2371f6097e5e5893961
Thorgalix <lucaspodevin@sfr.fr>
2025-12-01 11:46:08 +0100

    Ajoute la fonction add_node_end pour ajouter un nouveau nœud à la fin d'une liste chaînée

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * add_node_end - Adds a new node at the end of a list_t list.
+ * @head: Double pointer to the head of the list.
+ * @str: String to be added to the new node.
+ *
+ * Return: Address of the new element, or NULL if it failed.
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node;
+	list_t *last = *head;
+	unsigned int len = 0;
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	new_node->len = len;
+	new_node->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new_node;
+	}
+	else
+	{
+		while (last->next != NULL)
+		{
+			last = last->next;
+		}
+		last->next = new_node;
+	}
+	return (new_node);
+}
