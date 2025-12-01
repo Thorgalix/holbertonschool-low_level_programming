ba738a33ac8163656a9533e5d86178e68e38c9c7
Thorgalix <lucaspodevin@sfr.fr>
2025-12-01 14:05:37 +0100

    Ajoute des commentaires de documentation pour la fonction free_list

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,6 +1,9 @@
 #include <stdlib.h>
 #include "lists.h"
-
+/**
+ * free_list - frees a singly linked list
+ * @head: pointer to the head of the list
+ */
 void free_list(list_t *head)
 {
 list_t *tmp;
