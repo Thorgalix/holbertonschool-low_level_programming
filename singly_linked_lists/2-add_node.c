54aa7fcfd58abf7b21673ba060de007b68afe1a9
Thorgalix <lucaspodevin@sfr.fr>
2025-12-01 11:26:28 +0100

    Ajoute la fonction add_node pour ajouter un nouveau nœud au début d'une liste chaînée

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * add_node - Adds a new node at the beginning of a list_t list.
+ * @head: Double pointer to the head of the list.
+ * @str: String to be added to the new node.
+ *
+ * Return: Address of the new element, or NULL if it failed.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_first;
+
+	new_first = malloc(sizeof(list_t));
+	if (new_first == NULL)
+	{
+		return (NULL);
+	}
+	new_first->str = strdup(str);
+	if (new_first->str == NULL)
+	{
+		free(new_first);
+		return (NULL);
+	}
+	new_first->len = strlen(str);
+	new_first->next = *head;
+	*head = new_first;
+	return (new_first);
+}
