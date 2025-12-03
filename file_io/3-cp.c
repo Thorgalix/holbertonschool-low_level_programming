62b2badd9ae9bd0023a1e46cb3a41f580163ff6c
Thorgalix <lucaspodevin@sfr.fr>
2025-12-03 11:58:44 +0100

    Modification du programme pour respecter la norme betty

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -2,7 +2,6 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include "main.h"
 /**
  * main - copies the content of a file to another file
  * @argc: number of arguments
@@ -18,44 +17,37 @@ int main(int argc, char **argv)
 
 	if (argc != 3)
 	{
-		dprintf(2, "Usage: cp file_from file_to\n");
-		exit(97);
+		dprintf(2, "Usage: cp file_from file_to\n"), exit(97);
 	}
 	from = open(argv[1], O_RDONLY);
 	if (from == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
 	}
 	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (to == -1)
 	{
-		dprintf(2, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
+		dprintf(2, "Error: Can't write to %s\n", argv[2]), exit(99);
 	}
 	while ((r = read(from, buf, 1024)) > 0)
 	{
 		w = write(to, buf, r);
 		if (w != r)
 		{
-			dprintf(2, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
+			dprintf(2, "Error: Can't write to %s\n", argv[2]), exit(99);
 		}
 	}
 	if (r == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
 	}
 	if (close(from) == -1)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", from);
-		exit(100);
+		dprintf(2, "Error: Can't close fd %d\n", from), exit(100);
 	}
 	if (close(to) == -1)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", to);
-		exit(100);
+		dprintf(2, "Error: Can't close fd %d\n", to), exit(100);
 	}
 	return (0);
 }
