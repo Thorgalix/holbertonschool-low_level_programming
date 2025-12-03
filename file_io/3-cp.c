29302854e68f54ed7a4c03b206de3a077fc2bcd7
Thorgalix <lucaspodevin@sfr.fr>
2025-12-03 12:01:27 +0100

    Correctif pour le test STDOUT

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -22,7 +22,7 @@ int main(int argc, char **argv)
 	from = open(argv[1], O_RDONLY);
 	if (from == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
+		dprintf(1, "Error: Can't read from file %s\n", argv[1]), exit(98);
 	}
 	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (to == -1)
@@ -39,7 +39,7 @@ int main(int argc, char **argv)
 	}
 	if (r == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
+		dprintf(1, "Error: Can't read from file %s\n", argv[1]), exit(98);
 	}
 	if (close(from) == -1)
 	{
